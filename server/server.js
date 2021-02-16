d1c82bc078ad118f9b810e376e78a387ef70395e
wjdingman1 <wjdingman1@gmail.com>
2021-02-16 13:55:30 -0500

    Develop (#7)
    * Update howto.md
    
    * Fix vulnerabilities
    
    * Cleanup HowTo docs
    
    * Refactor/small cleanup (#6)
    
    * WIP
    
    * Extract express app and cleanup accessory files
    
    Co-authored-by: wdingman <wes.dingman@nextcentury.com>
    
    Co-authored-by: wdingman <wes.dingman@nextcentury.com>

--- server/server.js
+++ server/server.js
@@ -1,45 +1,15 @@
 const http = require("http");
-const path = require("path");
-
-const express = require("express");
-
-const cors = require("cors");
 const config = require("config");
-const routes = require("./routes");
-const bodyParser = require("body-parser");
 const healthcheck = require("./healthcheck");
 const findRemoveSync = require("find-remove");
-const { serializeError } = require("serialize-error");
-const { workingRoot, inboxPath } = require("./helpers/directories");
-
-const app = express();
+const { inboxPath } = require("./helpers/directories");
 const port = config.get("PORT");
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "static")));
-app.use("/", express.static(workingRoot));
-app.use("/", routes);
-
-//Log all the errors and send to the client
-app.use(function(err, req, res, next) {
-  console.error(err);
-  const error =
-    err instanceof Error || err instanceof TypeError
-      ? err
-      : new Error(err.message ? err.message : err);
-  delete error.stack;
-  delete error.metadata;
-  res.status(500);
-  res.json(serializeError(error));
-});
-
+/* Create express server */
+const app = require("./createExpress").generateExpress();
 const server = http.createServer(app);
 
-//healthcheck
+/* Setup healthcheck */
 healthcheck.setupHealthCheck(server);
 
 /* Remove videos from inbox every 10 minutes that are older than an hour
